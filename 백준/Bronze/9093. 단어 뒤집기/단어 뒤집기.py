3233c1dd1a3ab6f8e2c0eb167a75cb8e6b006430
Minjun Kim(김민준) <101498204+kidmillionaire1229@users.noreply.github.com>
2023-08-09 15:50:25 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 232 ms, Memory: 115504 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
@@ -1,42 +1,23 @@
 import sys 
 input = sys.stdin.readline 
 
-def reverseWord(word):
-    word = list(word)
-    str = ""
-    wordLength = len(word)
-    switchIndex = (wordLength-1)//2
-    #print(wordLength,switchIndex) 
-    for i in range(switchIndex+1): 
-        word[i],word[wordLength-1-i] = word[wordLength-1-i],word[i]
-    word = listToString(word)
-    return word 
-
-def listToString(list):
-    str = ""
-    for i in list: 
-        str+=i 
-    return str
-
 inputWordNum = int(input().rstrip())
 
-result = [[]for _ in range(inputWordNum)]
 
-for i in range(inputWordNum):
-    inputWord = input().rstrip().split()
-    for splitWord in inputWord: 
-        result[i].append(reverseWord(splitWord)) 
 
-#print(result)
+for _ in range(inputWordNum):
+    str = input().rstrip()
+    words = list(str.split(' '))
+    reversedWords = []
+    
+    for word in words:
+        reversedWords.append(word[::-1])
 
-for splitWords in result:
-    #print(splitWords,len(splitWords))
-    for index in range(len(splitWords)):
-        #print("see",index,splitWords[index],len(splitWords)-1) 
-        if index == (len(splitWords)-1): 
-            print(splitWords[index])
-        else: 
-            print(splitWords[index],end=" ")
+    answer = " ".join(reversedWords)
+    print(answer)
+
+
+
 
 
     
