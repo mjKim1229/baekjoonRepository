3173994a08cb5cdbe7a9543078376cbcd0a3c90e
Minjun Kim(김민준) <101498204+kidmillionaire1229@users.noreply.github.com>
2023-10-29 21:59:23 +0900

    [Silver IV] Title: 한수, Time: 112 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.py
+++ 백준/Silver/1065. 한수/한수.py
@@ -7,9 +7,7 @@ if x <= 99:
     print(x)
 else:
     for i in range(a,x+1):
-        first =  i//100 
-        second = i//10 - (first*10) 
-        third = i%10
-        if first - second == second - third:  
-            count +=1
-    print(count) 
+        nums = list(map(int, str(i)))
+        if nums[0] - nums[1] == nums[1] - nums[2]: 
+            count +=1 
+    print(count)     
