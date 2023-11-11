9d6b50117413f9ea573f9bc533984614ce1271cf
Minjun Kim(김민준) <101498204+kidmillionaire1229@users.noreply.github.com>
2023-11-12 00:53:46 +0900

    [Silver I] Title: 부등호, Time: 172 ms, Memory: 111864 KB -BaekjoonHub

---
+++ 백준/Silver/2529. 부등호/부등호.py
@@ -0,0 +1,37 @@
+import sys 
+input = sys.stdin.readline 
+
+k = int(input().rstrip())
+sign = list(input().rstrip().split())
+ 
+visited = [0] * 10 
+max_ans = ""
+min_ans = ""
+
+def check(i,j,k): 
+    if k == "<": 
+        return i < j 
+    else: 
+        return i > j 
+
+def solve(depth,s):
+    global max_ans, min_ans
+
+    if depth == (k+1): 
+        if len(min_ans) == 0: 
+            min_ans = s 
+        else: 
+            max_ans = s 
+        return 
+
+    for i in range(10):
+        if not visited[i]: 
+            if(depth==0 or check(s[-1],str(i),sign[depth-1])):
+                visited[i] = True 
+                solve(depth+1, s+str(i))
+                visited[i] = False
+
+solve(0,"")
+print(max_ans)
+print(min_ans)   
+
