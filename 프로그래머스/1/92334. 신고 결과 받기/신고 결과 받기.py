40ab8022c0f8827cc2f7e028fc57932e0b5354d9
Minjun Kim(김민준) <101498204+kidmillionaire1229@users.noreply.github.com>
2023-12-20 16:44:11 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,8 +1,10 @@
 def solution(id_list, report, k):
     answer = []
-    #신고자 : 신고 받은 애
+    #신고자 : 신고 받은 사람
     toDeclare = {id:[] for id in id_list}
+    #신고 받은 사람 : 신고자 
     fromDeclare = {id:[] for id in id_list}
+    #신고 받은 사람 : 신고 횟수 
     declareCount = {id:0 for id in id_list}
     
     for case in report: 
@@ -15,13 +17,14 @@ def solution(id_list, report, k):
     for a in declareCount: 
         declareCount[a] = len(fromDeclare[a])
     
-    toDeclareList = list(toDeclare.items())
-    
-    for i in toDeclareList: 
-        count = 0 
-        for j in i[1]:
-            if declareCount[j] >= k:
-                count += 1
+
+    for id in id_list:
+        count =0 
+        #declared는 id가 신고한 사람 리스트 
+        for declared in toDeclare[id]:
+            #declared가 얼마나 신고당했는지 조회  
+            if  declareCount[declared] >= k: 
+                count +=1
         answer.append(count)
             
     return answer
