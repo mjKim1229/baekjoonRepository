810adbe27643066232d57116bf069e2e09af9355
Minjun Kim(김민준) <101498204+kidmillionaire1229@users.noreply.github.com>
2023-12-26 12:35:26 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1164 ms, Memory: 159024 KB -BaekjoonHub

---
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -0,0 +1,42 @@
+import sys  
+input = sys.stdin.readline
+from collections import deque
+
+cityNum, roadNum, distance, startCity = map(int,input().rstrip().split())
+road = [[] for _ in range(cityNum+1)]
+distanceList = [-1] * (cityNum+1)
+
+for _ in range(roadNum): 
+    start, end = map(int,input().rstrip().split())
+    road[start].append(end)
+
+def bfs(road, startCity):
+    queue = deque([startCity])
+    distanceList[startCity] = 0 
+    while queue: 
+        city = queue.popleft()
+        #print(city,distanceList[city])
+        for i in road[city]: 
+            if distanceList[i] == -1: 
+                distanceList[i] = distanceList[city] +1 
+                queue.append(i)
+
+bfs(road,startCity)
+
+isZero = False 
+
+answer = []
+for i in range(1,cityNum+1): 
+    if distanceList[i] == distance:
+        #print(i,distanceList[i]) 
+        answer.append(i)
+
+if len(answer) == 0: 
+    isZero = True
+    
+if isZero:
+    print(-1) 
+else: 
+    answer.sort()
+    for i in answer: 
+        print(i)
