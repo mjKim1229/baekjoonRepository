a8b2e324cb0cd8229dee82274aa3788335cbcfd4
Minjun Kim(김민준) <101498204+kidmillionaire1229@users.noreply.github.com>
2023-12-28 11:02:13 +0900

    [Gold V] Title: 경쟁적 전염, Time: 184 ms, Memory: 116656 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
@@ -1,44 +1,34 @@
+import sys 
+input = sys.stdin.readline
 from collections import deque
 
-n, k = map(int, input().split())
-
-graph = [] # 전체 보드 정보를 담는 리스트
-data = [] # 바이러스에 대한 정보를 담는 리스트
+#nxn그래프, k초 후 
+n, virusNum = map(int, input().rstrip().split())
+board = []
+initVirus = []
 
 for i in range(n):
-    # 보드 정보를 한 줄 단위로 입력
-    graph.append(list(map(int, input().split())))
+    board.append(list(map(int,input().rstrip().split())))
     for j in range(n):
-        # 해당 위치에 바이러스가 존재하는 경우
-        if graph[i][j] != 0:
-            # (바이러스 종류, 시간, 위치 X, 위치 Y) 삽입
-            data.append((graph[i][j], 0, i, j))
+        if board[i][j] != 0:
+            initVirus.append((board[i][j],i,j,0))
 
-# 정렬 이후에 큐로 옮기기 (낮은 번호의 바이러스가 먼저 증식하므로)
-data.sort()
-q = deque(data)
- 
-target_s, target_x, target_y = map(int, input().split())
- 
-# 바이러스가 퍼져나갈 수 있는 4가지의 위치
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
+target_time, target_X, target_Y = map(int,input().rstrip().split())
+initVirus.sort()
 
-# 너비 우선 탐색(BFS) 진행
-while q:
-    virus, s, x, y = q.popleft()
-    # 정확히 s초가 지나거나, 큐가 빌 때까지 반복
-    if s == target_s:
+destinationX = [0,1,0,-1]
+destinationY = [1,0,-1,0]
+
+q = deque(initVirus)
+while q: 
+    virus,x,y,time = q.popleft()
+    if time == target_time:
         break
-    # 현재 노드에서 주변 4가지 위치를 각각 확인
     for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        # 해당 위치로 이동할 수 있는 경우
-        if 0 <= nx and nx < n and 0 <= ny and ny < n:
-            # 아직 방문하지 않은 위치라면, 그 위치에 바이러스 넣기
-            if graph[nx][ny] == 0:
-                graph[nx][ny] = virus
-                q.append((virus, s + 1, nx, ny))
+        tempX = x + destinationX[i]
+        tempY = y + destinationY[i]
+        if 0<=tempX<n and 0<=tempY<n and board[tempX][tempY] == 0:
+            board[tempX][tempY] = virus
+            q.append((board[tempX][tempY],tempX,tempY,time+1))
 
-print(graph[target_x - 1][target_y - 1])
+print(board[target_X-1][target_Y-1])
