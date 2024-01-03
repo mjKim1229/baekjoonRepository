f0c890467f86d4b5e85773323c850587871d0bce
Minjun Kim(김민준) <101498204+kidmillionaire1229@users.noreply.github.com>
2024-01-03 16:26:24 +0900

    [Silver III] Title: N과 M (1), Time: 148 ms, Memory: 110596 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,21 +1,13 @@
+from itertools import permutations
 import sys 
 input = sys.stdin.readline 
 
 n , m = map(int,input().rstrip().split())
-rs = []
-visited = [False] * (n+1)
 
-def recur(num):
-    if num == m: 
-        print(' '.join(map(str,rs)))
-        return 
-    for i in range(1,n+1): 
-        if not visited[i]:
-            visited[i] = True  
-            rs.append(i)
-            recur(num+1)
-            visited[i] = False
-            rs.pop()
-    
+numbers = [i for i in range(1,n+1)]
 
-recur(0)
+for numberPick in permutations(numbers,m):
+    rs = []
+    for num in numberPick:
+        rs.append(num)
+    print(' '.join(map(str,rs)))
