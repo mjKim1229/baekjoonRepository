1400ddaa73b0f86ae12c5167432f76ebe3f76c8e
Minjun Kim(김민준) <101498204+kidmillionaire1229@users.noreply.github.com>
2024-01-07 21:51:07 +0900

    [Silver V] Title: 숫자 카드, Time: 1296 ms, Memory: 218208 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -0,0 +1,24 @@
+import sys 
+input = sys.stdin.readline 
+
+n = int(input().rstrip())
+haveList = sorted(list(map(int,input().rstrip().split())))
+
+m = int(input().rstrip())
+checkList = list(map(int,input().rstrip().split()))
+#print(haveList)
+def binarySearch(array, target,start,end):
+    #print(start,end)
+    if start > end: 
+        return 0 
+    mid = (start+end)//2
+    if array[mid] == target:
+        #print("find",target,mid)
+        return 1 
+    elif array[mid] > target: 
+        return binarySearch(array,target,start,mid-1)  
+    else: 
+        return binarySearch(array,target,mid+1,end)
+
+for num in checkList:
+    print(binarySearch(haveList, num,0,n-1),end=" ")
