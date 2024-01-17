39c8369584c13ce8f1023c05e371d40db92d57b8
Minjun Kim(김민준) <101498204+kidmillionaire1229@users.noreply.github.com>
2024-01-17 14:48:46 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -0,0 +1,14 @@
+import sys 
+input = sys.stdin.readline 
+
+n = int(input().rstrip())
+
+dp = [0] * 1001 
+dp[1] = 1 
+dp[2] = 3 
+
+for i in range(3,n+1):
+    dp[i] = dp[i-1] + dp[i-2] * 2 
+
+#print(dp)
+print(dp[n]%10007)
