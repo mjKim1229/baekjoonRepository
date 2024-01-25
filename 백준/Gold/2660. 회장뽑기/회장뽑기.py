3a315b6d5b205d8ee830de984e66d2d400d4b4f4
Minjun Kim(김민준) <101498204+kidmillionaire1229@users.noreply.github.com>
2024-01-25 10:05:34 +0900

    [Gold V] Title: 회장뽑기, Time: 152 ms, Memory: 113164 KB -BaekjoonHub

---
+++ 백준/Gold/2660. 회장뽑기/회장뽑기.py
@@ -0,0 +1,54 @@
+import sys 
+input = sys.stdin.readline 
+from collections import deque
+
+def bfs(target,visited):
+    tempMaxDepth = 0 
+    visited[target] = True 
+    q = deque()
+    for friend in friends[target]:
+        visited[friend] = True  
+        q.append( (friend, 1) )
+    while q: 
+        friend, depth = q.popleft()
+        tempMaxDepth = max(depth, tempMaxDepth)
+        for i in friends[friend]:
+            if not visited[i]:
+                visited[i] = True 
+                q.append( (i, depth+1) )
+    return tempMaxDepth
+
+n = int(input().rstrip())
+friends =[ [] for _ in range(n+1) ]
+maxDepth = [0] * (n+1)
+
+while True: 
+    a, b = map(int, input().rstrip().split())
+    if a == -1 and b == -1:
+        break 
+    friends[a].append(b)
+    friends[b].append(a)
+
+for i in range(1,n+1):
+    visited = [False] * (n+1)
+    tempMaxDepth = bfs(i, visited)
+    maxDepth[i] = tempMaxDepth
+
+
+INF = int(1e9)
+minScore = INF 
+
+for i in range(1,n+1):
+    if maxDepth[i] != 0: 
+        minScore = min(minScore, maxDepth[i])
+
+captainNum = 0 
+captains = []
+for i in range(1,n+1):
+    if maxDepth[i] == minScore:
+        captainNum +=1 
+        captains.append(i)
+
+print(minScore, captainNum)
+print(' '.join(map(str, captains)))
+
