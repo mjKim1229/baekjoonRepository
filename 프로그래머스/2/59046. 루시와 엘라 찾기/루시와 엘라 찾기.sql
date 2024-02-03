ab72639bf35b64e094aac0380a134165eb048e19
Minjun Kim(김민준) <101498204+kidmillionaire1229@users.noreply.github.com>
2024-02-03 10:04:38 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -3,3 +3,8 @@ SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
 FROM ANIMAL_INS
 WHERE NAME in ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
 ORDER BY ANIMAL_ID
+
+# SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
+# FROM ANIMAL_INS
+# WHERE NAME in ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
+# ORDER BY ANIMAL_ID
