b3058d4f71bb3008ccb28c49b1afee8cfc0a9847
Minjun Kim(김민준) <101498204+kidmillionaire1229@users.noreply.github.com>
2024-02-05 20:06:19 +0900

    [Silver I] Title: 스타트와 링크, Time: 600 ms, Memory: 137228 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,34 +1,29 @@
 import sys 
+input = sys.stdin.readline
 from itertools import combinations
-input = sys.stdin.readline 
 
 n = int(input().rstrip())
-array = []
-totalSum = 0 
-INF = int(1e9)
-gap = INF 
+graph = []
 for _ in range(n):
     row = list(map(int, input().rstrip().split())) 
-    totalSum += sum(row)
-    array.append(row)
+    graph.append(row)
 
-totalPersonNum = set([x for x in range(n)]) 
-refNums = [x for x in range(n)]
-#print(totalSum)
-choices = list(combinations(refNums,n//2))
+allNum = [x for x in range(n)]
+pickList = list(combinations(allNum, n//2)) 
 
-for firstTeamChoice in choices: 
-    firstTeamScore = 0 
-    for x,y in combinations(firstTeamChoice, 2):
-        firstTeamScore += (array[x][y] + array[y][x] )
-    secondTeamChoice = list(totalPersonNum - set(firstTeamChoice)) 
-    secondTeamScore = 0 
-    for a,b in combinations(secondTeamChoice,2):
-        secondTeamScore += (array[a][b] + array[b][a])
-    tempGap = abs(firstTeamScore - secondTeamScore)
-    gap = min(tempGap, gap)
-
-print(gap)
-
-
+answer = int(1e9)
+for i in range(len(pickList)//2):
+    allNumSet = set(allNum)
+    firstTeam = list(pickList[i]) 
+    firstTeamSum = 0 
+    for a,b in combinations(firstTeam,2):
+        firstTeamSum += (graph[a][b] + graph[b][a])
     
+    secondTeamSum = 0 
+    secondTeam = list(allNumSet - set(firstTeam)) 
+    for a,b in combinations(secondTeam,2):
+        secondTeamSum += (graph[a][b] + graph[b][a])
+
+    answer = min(abs(firstTeamSum-secondTeamSum), answer)
+
+print(answer)
