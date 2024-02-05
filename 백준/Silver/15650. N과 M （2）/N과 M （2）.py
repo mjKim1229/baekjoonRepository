d241887c514ab56d35c0143c293bad015d814ec3
Minjun Kim(김민준) <101498204+kidmillionaire1229@users.noreply.github.com>
2024-02-05 15:10:24 +0900

    [Silver III] Title: N과 M (2), Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,23 +1,31 @@
 import sys 
 input = sys.stdin.readline 
+from itertools import combinations
 
 n, m = map(int, input().rstrip().split())
 
-rs = []
-visited = [False] * (n+1)
+#백트래킹 
+# rs = []
+# visited = [False] * (n+1)
 
-def recur(count, num):
-    if count == m:
-        print(' '.join(map(str,rs))) 
-        return
-    for i in range(n+1):
-        if i > num and not visited[i]:
-            visited[i] = True
-            rs.append(i)
-            recur(count+1,i)
-            visited[i] = False
-            rs.pop() 
+# def recur(count, num):
+#     if count == m:
+#         print(' '.join(map(str,rs))) 
+#         return
+#     for i in range(n+1):
+#         if i > num and not visited[i]:
+#             visited[i] = True
+#             rs.append(i)
+#             recur(count+1,i)
+#             visited[i] = False
+#             rs.pop() 
              
+# recur(0,0)
 
+#itertools 
+array = [x for x in range(1,n+1)]
 
-recur(0,0)
+for answer in combinations(array,m):
+    for i in answer:
+        print(i,end=" ")
+    print()
