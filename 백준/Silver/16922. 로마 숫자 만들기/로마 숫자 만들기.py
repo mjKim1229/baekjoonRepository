49573f7f92ce0ed6ae8f5b393110f77e4f4a26fc
Minjun Kim(김민준) <101498204+kidmillionaire1229@users.noreply.github.com>
2024-02-05 20:25:13 +0900

    [Silver III] Title: 로마 숫자 만들기, Time: 148 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/16922. 로마 숫자 만들기/로마 숫자 만들기.py
+++ 백준/Silver/16922. 로마 숫자 만들기/로마 숫자 만들기.py
@@ -1,19 +1,21 @@
-def recur(cur, start):
-    global num
-    if cur == n:
-        arr[num] = 1
-        return
- 
- 
-    for i in range(start, 4):
-        num += li[i]
-        recur(cur + 1, i)
-        num -= li[i]
- 
- 
-n = int(input())
-li = [1, 5, 10, 50]
-arr = [0] * (50 * 20 + 1)
-num = 0
-recur(0, 0)
-print(sum(arr))
+import sys 
+input = sys.stdin.readline 
+from itertools import combinations_with_replacement
+# sys.setrecursionlimit(10**6)
+m = int(input().rstrip())
+
+
+numList = [1, 5, 10, 50]
+sumList = [0] * 1001
+
+totalNum = [x for x in range(4)]
+numPicks = list(combinations_with_replacement(totalNum,m)) 
+
+for pick in numPicks:
+    tempsum = 0 
+    for i in range(len(pick)):
+        index = pick[i]
+        tempsum += numList[index]
+    sumList[tempsum] = 1 
+
+print(sum(sumList)) 
