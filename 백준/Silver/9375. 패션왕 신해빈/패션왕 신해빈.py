eec610af39ecbf4c7e87d475bed2986285ed7693
Minjun Kim(김민준) <101498204+kidmillionaire1229@users.noreply.github.com>
2024-02-05 19:31:42 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 132 ms, Memory: 112004 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.py
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.py
@@ -1,18 +1,17 @@
 import sys 
 input = sys.stdin.readline 
-from collections import defaultdict
+from collections import defaultdict 
 
-for _ in range(int(input())):
-    clothNum = int(input())
-    if clothNum == 0: 
-        print(0)
-    else:
-        clothDict = defaultdict(int) 
-        for _ in range(clothNum):
-            answer = 1  
-            clothe , kind = input().rstrip().split()
-            clothDict[kind] += 1
-        for value in clothDict.values(): 
-            answer *= (value+1)
-        print(answer -1) 
+#itertools 
+for _ in range(int(input().rstrip())):
+    closet = defaultdict(int)
+    n = int(input().rstrip())
+    for _ in range(n):
+        a, b =  input().rstrip().split()
+        closet[b] += 1 
     
+
+    answer = 1 
+    for key in closet.keys():
+        answer *= (closet[key] + 1)
+    print(answer-1)
