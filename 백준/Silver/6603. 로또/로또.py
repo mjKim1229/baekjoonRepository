c9e66bf340323da45ed926475f1e8429ca4fe8f2
Minjun Kim(김민준) <101498204+kidmillionaire1229@users.noreply.github.com>
2024-02-05 18:40:04 +0900

    [Silver II] Title: 로또, Time: 124 ms, Memory: 110420 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.py
+++ 백준/Silver/6603. 로또/로또.py
@@ -1,27 +1,44 @@
 import sys 
 input = sys.stdin.readline 
 
-res = []
-def recur(start, count):
-    if count == 6: 
-        print(' '.join(map(str,res))) 
-        return  
-    for i in range(start,pick+1):
-        res.append(lotto[i])
-        recur(i+1,count+1) 
-        res.pop()
+from itertools import combinations
 
+#print(totalList)
 
-lotto = []
-pick = 0
-while(True):
-    lotto = list(map(int,input().rstrip().split()))
-    if lotto[0] == 0: 
+while True:
+    inputList = list(map(int, input().rstrip().split())) 
+    pickNum = inputList[0]
+    inputList = inputList[1:]
+    if pickNum == 0:
         break
-    pick = lotto[0] 
-    recur(1,0)
-    print()
 
-# lotto = list(map(int,input().rstrip().split()))
-# pick = lotto[0]
-# recur(1,0)
+    pickList = list(combinations(inputList,6)) 
+
+    for pick in pickList:
+        print(' '.join(map(str, pick)))
+    print()
+#백트래킹 
+# res = []
+# def recur(start, count):
+#     if count == 6: 
+#         print(' '.join(map(str,res))) 
+#         return  
+#     for i in range(start,pick+1):
+#         res.append(lotto[i])
+#         recur(i+1,count+1) 
+#         res.pop()
+
+
+# lotto = []
+# pick = 0
+# while(True):
+#     lotto = list(map(int,input().rstrip().split()))
+#     if lotto[0] == 0: 
+#         break
+#     pick = lotto[0] 
+#     recur(1,0)
+#     print()
+
+# # lotto = list(map(int,input().rstrip().split()))
+# # pick = lotto[0]
+# # recur(1,0)
