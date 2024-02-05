c4286cc3ea6ac99d5a204fc82ebeacdd5ddc9979
Minjun Kim(김민준) <101498204+kidmillionaire1229@users.noreply.github.com>
2024-02-05 15:36:29 +0900

    [Silver III] Title: N과 M (7), Time: 680 ms, Memory: 119828 KB -BaekjoonHub

--- 백준/Silver/15656. N과 M （7）/N과 M （7）.py
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -1,18 +1,32 @@
 import sys 
 input = sys.stdin.readline 
+from itertools import product
 
-# 1 1 2 3 
 n, m = map(int,input().rstrip().split())
-numList = sorted(set(list(map(int,input().rstrip().split()))))
-rs = []
+numList = sorted(list(map(int,input().rstrip().split())))
 
-def rec(num):
-    if num == m:
-        print(' '.join(map(str,rs))) 
-        return
-    for i in range(n):
-        rs.append(numList[i])
-        rec(num+1)
-        rs.pop() 
+#백트래킹 
+# visited = [False] * n 
+# rs = []
 
-rec(0)
+# def rec(num, mark):
+#     #print("check",num,mark)
+#     if num == m:
+#         print(' '.join(map(str,rs))) 
+#         return
+#     mark = 0 
+#     for i in range(n):
+#         if not visited[i] and mark!=numList[i]: 
+#             rs.append(numList[i])
+#             visited[i] = True 
+#             mark = numList[i]
+#             rec(num+1, mark)
+#             visited[i]= False 
+#             rs.pop() 
+# rec(0,0)
+
+#itertools 
+for answer in product(numList,repeat = m):
+    for i in answer: 
+        print(i,end=" ")
+    print()
