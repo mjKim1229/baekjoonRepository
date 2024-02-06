bf8a2cefa22eb78014419e79bd7240ca0401d648
Minjun Kim(김민준) <101498204+kidmillionaire1229@users.noreply.github.com>
2024-02-06 21:17:38 +0900

    [Gold V] Title: 감시 피하기, Time: 144 ms, Memory: 111356 KB -BaekjoonHub

--- 백준/Gold/18428. 감시 피하기/감시 피하기.py
+++ 백준/Gold/18428. 감시 피하기/감시 피하기.py
@@ -1,79 +1,50 @@
-from itertools import combinations
-from collections import deque
+#S: 학생, T: 선생 , O: 벽 
 import sys 
 input = sys.stdin.readline 
-
-
+from itertools import combinations
 n = int(input().rstrip())
-graph = []
-teacher = []
-spaces = []
-for i in range(n): 
-    graph.append(list(input().rstrip().split()))
+graph = [list(input().rstrip().split()) for _ in range(n)]
+teachers = []
+for i in range(n):
     for j in range(n):
-        if graph[i][j] == 'T': 
-            teacher.append((i,j))
-        if graph[i][j] == 'X':
-            spaces.append((i,j))
+        if graph[i][j] == 'T':
+            teachers.append((i,j))
 
-#특정 방향으로 감시를 진행 (True: 학생발견, False: 학생 미발견)
-def watch(x,y,direction): 
-    #왼쪽 
-    if direction == 0: 
-        while y >=0:
-            if graph[x][y] == "S":
-                return True 
-            if graph[x][y] == "O":
-                return False
-            y -=1  
-    #오른쪽
-    if direction == 1:
-        while y<n:
-            if graph[x][y] == "S":
-                return True 
-            if graph[x][y] == "O":
-                return False 
-            y +=1 
-    #위쪽 
-    if direction == 2: 
-        while x >=0:
-            if graph[x][y] == "S":
-                return True 
-            if graph[x][y] == "O":
-                return False 
-            x -=1 
-    #아래 
-    if direction == 3: 
-        while x <n: 
-            if graph[x][y] == "S":
-                return True 
-            if graph[x][y] == "O":
-                return False 
-            x +=1 
-    return False
 
-#장애물 설치 이후에, 한명이라도 학생을 찾을 수 있는지 (True: 한명이라도 검사 False: 한명도 못 찾음)
-def process():
-    for x,y in teacher:
-        for i in range(4):
-            if watch(x,y,i):
-                return True 
-    return False 
+totalNum = [(x,y) for x in range(n) for y in range(n) if graph[x][y] == 'X']
+combi = list(combinations(totalNum,3)) 
 
-find = False 
-for data in combinations(spaces,3): 
-    #벽 세움 
-    for x,y in data:
+dx = [0,1,0,-1]
+dy = [1,0,-1,0]
+
+
+for pick in combi:
+    avoid = True
+    #벽 세우기 
+    for x,y in pick:
         graph[x][y] = 'O'
-    #학생이 한명도 감지 x 
-    if not process():
-        find = True 
-        break 
-    #장애물 회수 
-    for x,y in data:
+    #선생님 별 탐색 
+    for tx, ty in teachers:
+        for i in range(4):
+            xx , yy = tx, ty 
+            while True:
+                xx += dx[i]
+                yy += dy[i]
+                #index out 
+                if xx<0 or xx>=n or yy<0 or yy>=n:
+                    break 
+                #벽 
+                if graph[xx][yy] == 'O':
+                    break 
+                #학생 
+                if graph[xx][yy] == 'S':
+                    avoid = False
+    
+    if avoid == True: 
+        print("YES")
+        exit(0)
+    #원래대로 빈칸 복구 
+    for x,y in pick:
         graph[x][y] = 'X'
-
-if find: 
-    print("YES")
-else: 
-    print("NO")
+    
+print("NO")
