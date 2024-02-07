9e329611f8d1f6a24d12ffad0da144cba7bc8244
Minjun Kim(김민준) <101498204+kidmillionaire1229@users.noreply.github.com>
2024-02-07 16:58:53 +0900

    [Gold IV] Title: 소트 게임, Time: 272 ms, Memory: 124772 KB -BaekjoonHub

--- 백준/Gold/1327. 소트 게임/소트 게임.py
+++ 백준/Gold/1327. 소트 게임/소트 게임.py
@@ -1,33 +1,36 @@
+import sys 
+input = sys.stdin.readline 
 from collections import deque
-import sys
-input = sys.stdin.readline
 
+#n개의 수, k개를 오른쪽으로 뒤집어야함 
 n, k = map(int, input().rstrip().split())
 
-array = list(input().rstrip().split())
-visited = set("".join(array))
-
+array = list(input().rstrip().split()) 
 target = sorted(array)
 
+visited = set(''.join(array))
+
 q = deque()
-q.append([array,0])
+q.append((array,0))
 
 answer = -1 
-
-while q: 
-    arr, cnt = q.popleft()
-    if arr == target: 
-        answer = cnt 
+while q:
+    arr, time = q.popleft()
+    if arr == target:
+        answer = time 
         break 
+    # i ~ i+(k-1) // i + k -1 = n-1 // i = n-k까지니까   
     for i in range(n-k+1):
         rarr = arr[i:i+k]
         rarr.reverse()
         arr2 = arr[:i] + rarr + arr[i+k:]
         str2 = ''.join(arr2)
         if str2 not in visited:
-            q.append([arr2,cnt+1])
             visited.add(str2)
-        
+            q.append((arr2,time+1))
+    
+
 
 print(answer)
+
 
