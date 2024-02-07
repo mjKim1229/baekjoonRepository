7fa16af75f81808bebf27af0b3b86a41a77dd9c6
Minjun Kim(김민준) <101498204+kidmillionaire1229@users.noreply.github.com>
2024-02-07 21:16:29 +0900

    [Gold IV] Title: 연구소, Time: 708 ms, Memory: 120244 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,61 +1,61 @@
-#0: 빈칸 1: 벽 2: 바이러스
 import sys 
 input = sys.stdin.readline 
+from collections import deque
 from itertools import combinations
-#방향 
-destinationX = [0,1,0,-1]
-destinationY = [1,0,-1,0]
+import copy
+#0: 빈칸 , 1: 벽 2: 바이러스 
+n, m = map(int, input().rstrip().split())
+graph = [list(map(int, input().rstrip().split())) for _ in range(n)]
 
-#입력 
-row, column = map(int,input().rstrip().split())
-graph = []
+#virus 저장 
+virus = [(x,y) for x in range(n) for y in range(m) if graph[x][y] == 2]
+blanks = [(x,y) for x in range(n) for y in range(m) if graph[x][y] ==0]
 
-for _ in range(row):
-    tempRow = list(map(int,input().rstrip().split())) 
-    graph.append(tempRow)
+wallcombi = list(combinations(blanks,3)) 
+dx = [0,1,0,-1]
+dy = [1,0,-1,0]
+answer = 0 
 
-#벽 세울 위치 3곳 선택 
-wallCanList = [(x,y) for x in range(row) for y in range(column) if graph[x][y] ==0]
-wallPickThree = list(combinations(wallCanList,3))
-
-#바이러스 위치 
-virusList = []
-for i in range(row): 
-    for j in range(column): 
-        if graph[i][j] == 2: 
-            virusList.append((i,j))
-
-#탐색 
-def dfs(graph,x,y,temp):
-    temp[x][y] = 2    
-    for i in range(4): 
-        tempX = x + destinationX[i]
-        tempY = y + destinationY[i]
-        if 0<=tempX<row and 0<=tempY<column and not temp[tempX][tempY] and temp[tempX][tempY]!=1: 
-            dfs(graph,tempX,tempY,temp)
-
-maxCount = 0 
-for threeWall in wallPickThree:
-    count = 0 
-    temp = [[0]* column for _ in range(row)]
-    #기존 graph 정보 옮겨 담기 
-    for i in range(row):
-        for j in range(column): 
-            temp[i][j] = graph[i][j] 
+for pick in wallcombi:
+    
+    #안전 영역 
+    safeCount = 0 
+    #temp로 바이러스 전파 
+    temp = copy.deepcopy(graph)
+    visited = [[False] * m for _ in range(n)]
+    
     #벽 세우기 
-    #print("wall pick",threeWall)
-    #새로운 벽 
-    for x,y in threeWall:
-        temp[x][y] = 1
-    #print("벽 세워짐",temp) 
-    #바이러스 꺼내서 dfs 
-    for x,y in virusList:
-        dfs(graph,x,y,temp)
-    #print("탐색 완료",temp)
-    for i in range(row): 
-        for j in range(column): 
-            if temp[i][j] == 0: 
-                count +=1 
-    maxCount = max(count,maxCount)
+    for x, y in pick:
+        temp[x][y] = 1 
+    
+    #초기 바이러스 
+    q = deque()
+    for x, y in virus:
+        q.append((x,y))
+        visited[x][y] = True 
 
-print(maxCount)
+    #bfs 
+    while q: 
+        x, y = q.popleft()
+        for i in range(4):
+            xx = x + dx[i]
+            yy = y + dy[i]
+            if 0<=xx<n and 0<=yy<m and not visited[xx][yy]:
+                #벽 말고 빈칸, 바이러스는 확산 가능 
+                if temp[xx][yy] != 1:
+                    visited[xx][yy] = True
+                    temp[xx][yy] = 2
+                    q.append((xx,yy))
+    
+    #안전지대 count 
+    for i in range(n):
+        for j in range(m):
+            if temp[i][j] == 0:
+                safeCount +=1 
+    answer = max(answer, safeCount)
+  
+    #벽 원상태로 
+    for x,y in pick:
+        temp[x][y] = 0 
+
+print(answer)
