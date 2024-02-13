a531ef59f858dfd7e802909fe108e6e3f18488e3
Minjun Kim(김민준) <101498204+kidmillionaire1229@users.noreply.github.com>
2024-02-13 15:28:57 +0900

    [Gold V] Title: 상어 초등학교, Time: 272 ms, Memory: 115728 KB -BaekjoonHub

--- 백준/Gold/21608. 상어 초등학교/상어 초등학교.py
+++ 백준/Gold/21608. 상어 초등학교/상어 초등학교.py
@@ -1,56 +1,72 @@
 import sys 
 input = sys.stdin.readline 
 from collections import defaultdict
+#교실 N X N 
+#학생수 N제곱 
+#학생 번호 1 ~ N제곱 
+# (1,1) ~ (N,N)
+# 학생 - 좋아하는 학생 4명 
+# |r1 - r2| + |c1 - c2| = 1
 
+#비어있는 칸 중에 좋아하는 학생이 가장 많은 칸으로 
+#1이 여러개라면 비어있는 칸이 많은 칸으로 
+# 행 작은칸 => 열 작은칸 
 
 n = int(input().rstrip())
-favoriteList = defaultdict(list)
+favorites = defaultdict(list)
 
 for _ in range(n*n):
     temp = list(map(int, input().rstrip().split())) 
-    favoriteList[temp[0]] = temp[1:]
+    favorites[temp[0]] = temp[1:]
 
-board = [ [0] * n for _ in range(n) ] 
-#print(favoriteList)
+graph = [[0] * n for _ in range(n)]
+
 
 dx = [0,1,0,-1]
 dy = [1,0,-1,0]
 
-for person in favoriteList.keys():
-    locationList = []
+def singleSeat(i,j,student,friends):
+    favNum = 0 
+    blankNum = 0
+    for k in range(4):
+        xx = i + dx[k]
+        yy = j + dy[k]
+        if xx<0 or xx>=n or yy<0 or yy>=n:
+            continue 
+        if graph[xx][yy] in friends:
+            favNum +=1
+        elif graph[xx][yy] == 0:
+            blankNum +=1 
+    return (favNum, blankNum, i,j)
+
+
+for student, friends in favorites.items():
+    #print(student,"시작")
+    seats = []
     for i in range(n):
         for j in range(n):
-            if board[i][j] == 0:
-                favoriteCount = 0 
-                blankCount = 0 
-                for k in range(4):                
-                    tempX = i + dx[k]
-                    tempY = j + dy[k]
-                    if 0<= tempX <n and 0<= tempY <n:
-                        if board[tempX][tempY] in favoriteList[person]: 
-                            favoriteCount +=1
-                        elif board[tempX][tempY] == 0: 
-                            blankCount +=1
-                locationList.append((favoriteCount,blankCount,i,j))
-    locationList.sort(key=lambda x: (-x[0],-x[1],x[2],x[3]) )
-    board[locationList[0][2]][locationList[0][3]] = person 
+            if graph[i][j] == 0:
+                seat = singleSeat(i,j,student,friends)
+                seats.append(seat)
+
+    seats.sort(key= lambda x: (-x[0],-x[1],x[2],x[3]))
+    pick = seats[0]
+    #print(student,pick)
+    graph[pick[2]][pick[3]] = student
 
 
 #만족도 구하기 
+love = {0:0, 1:1, 2:10, 3:100, 4:1000}
 answer = 0 
 for i in range(n):
     for j in range(n):
-        tempFavoriteAnswer = 0 
+        count = 0 
+        student = graph[i][j]
         for k in range(4):
-            tempX = i + dx[k]
-            tempY = j + dy[k]
-            if 0<=tempX<n and 0<= tempY <n:
-                if board[tempX][tempY] in favoriteList[board[i][j]]:
-                    tempFavoriteAnswer +=1
-        #print(board[i][j],tempFavoriteAnswer)
-        if tempFavoriteAnswer > 0:
-            answer += (10**(tempFavoriteAnswer-1))
-
+            xx = i + dx[k]
+            yy = j + dy[k]
+            if 0<= xx <n and 0<= yy<n: 
+                if graph[xx][yy] in favorites[student]:
+                    count +=1 
+        answer += love[count]
 print(answer)
-        
-
