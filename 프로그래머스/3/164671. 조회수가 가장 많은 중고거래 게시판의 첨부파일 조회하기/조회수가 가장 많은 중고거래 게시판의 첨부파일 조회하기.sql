0c1f0c1c82983ad929669ac38eda66568360c8b7
Minjun Kim(김민준) <101498204+kidmillionaire1229@users.noreply.github.com>
2024-02-14 14:31:04 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,19 +1,17 @@
 -- 코드를 입력하세요
-SELECT CONCAT("/home/grep/src/",BOARD_ID,"/",FILE_ID,FILE_NAME,FILE_EXT) 
-AS FILE_PATH
+#조회수 가장 높은 중고 거래 게시물에 대한 첨부 파일 경로 
+#첨부파일 경로 : FILE ID 기준 내림차순 
+#기본 경로 : /home/grep/src/
+#게시글 ID로 디렉토리 경로 
+#파일 이름 : 파일 ID , 이름, 확장자 
+
+SELECT CONCAT("/home/grep/src/",BOARD_ID,"/",FILE_ID,FILE_NAME,FILE_EXT) AS FILE_PATH
 FROM USED_GOODS_FILE
-WHERE BOARD_ID = (
+WHERE BOARD_ID 
+= (
     SELECT BOARD_ID 
     FROM USED_GOODS_BOARD
     ORDER BY VIEWS DESC 
-    LIMIT 1 
-)
+    LIMIT 1
+    )
 ORDER BY FILE_ID DESC 
-
-
-# SELECT CONCAT('/home/grep/src/',b.BOARD_ID, '/', FILE_ID, FILE_NAME, FILE_EXT) AS FILE_PATH
-# FROM USED_GOODS_BOARD AS a 
-# INNER JOIN USED_GOODS_FILE AS b 
-# ON a.BOARD_ID = b.BOARD_ID
-# WHERE VIEWS = (SELECT max(VIEWS) FROM USED_GOODS_BOARD)
-# ORDER BY FILE_ID DESC
