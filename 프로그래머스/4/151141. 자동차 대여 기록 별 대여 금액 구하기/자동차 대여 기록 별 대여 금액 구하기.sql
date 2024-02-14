c89ba5080f8822e818a1a03e91f5ee319d34fc49
Minjun Kim(김민준) <101498204+kidmillionaire1229@users.noreply.github.com>
2024-02-14 09:47:14 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,22 +1,24 @@
-WITH value AS (
-    SELECT car.daily_fee, car.car_type, his.history_id,
-           DATEDIFF(end_date, start_date) + 1 AS period,
+#자동차 종류 트럭 
+#대여 기록 별 대여 금액 
+#대여 금액 내림차순, 대여 기록 id 내림차순 
+
+WITH DURATION AS 
+(
+    SELECT HISTORY_ID, CAR_ID, DAILY_FEE, DATEDIFF(END_DATE,START_DATE) +1 AS GAP, 
     CASE 
-      WHEN DATEDIFF(end_date, start_date) + 1 >= 90 THEN '90일 이상'
-      WHEN DATEDIFF(end_date, start_date) + 1 >= 30 THEN '30일 이상'
-      WHEN DATEDIFF(end_date, start_date) + 1 >= 7 THEN '7일 이상'
-      ELSE 'NONE' END AS duration_type
-FROM car_rental_company_rental_history AS his
-INNER JOIN car_rental_company_car AS car ON car.car_id = his.car_id
-WHERE car.car_type = '트럭')   
+        WHEN DATEDIFF(END_DATE,START_DATE) +1>= 90 THEN '90일 이상'
+        WHEN DATEDIFF(END_DATE,START_DATE) +1>=30 THEN '30일 이상'
+        WHEN DATEDIFF(END_DATE,START_DATE) +1 >= 7 THEN '7일 이상'
+        ELSE NULL END AS DURATION_TYPE, CAR_TYPE
+    FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+    INNER JOIN CAR_RENTAL_COMPANY_CAR
+    USING (CAR_ID)
+    WHERE CAR_TYPE = '트럭'
+) 
 
-
-
-SELECT value.history_id, 
-    ROUND(value.daily_fee * value.period * 
-          (100 - IFNULL(plan.discount_rate,0)) / 100) AS FEE
-FROM value
-LEFT JOIN car_rental_company_discount_plan AS plan 
-    ON plan.duration_type = value.duration_type 
-    AND plan.car_type = value.car_type
-ORDER BY 2 DESC, 1 DESC
+SELECT HISTORY_ID, ROUND(DAILY_FEE * GAP * (
+    (100-IFNULL(DISCOUNT_RATE,0))/100)) AS FEE 
+FROM DURATION 
+LEFT OUTER JOIN CAR_RENTAL_COMPANY_DISCOUNT_PLAN P 
+USING (DURATION_TYPE, CAR_TYPE)
+ORDER BY FEE DESC, HISTORY_ID DESC 
