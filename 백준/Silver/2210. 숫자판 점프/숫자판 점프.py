7065fe291c139cf18a2214cb8497b800134f510c
Minjun Kim(김민준) <101498204+kidmillionaire1229@users.noreply.github.com>
2024-02-20 16:30:40 +0900

    [Silver II] Title: 숫자판 점프, Time: 172 ms, Memory: 111544 KB -BaekjoonHub

--- 백준/Silver/2210. 숫자판 점프/숫자판 점프.py
+++ 백준/Silver/2210. 숫자판 점프/숫자판 점프.py
@@ -1,36 +1,39 @@
+#5 X 5 숫자판 
+#숫자 (0~9) 적혀져있음 
+#인접 4방향으로 이동 
+#5번 이동 
+#칸에 있는 숫자 붙임 
 import sys 
 input = sys.stdin.readline 
-import copy
 
-def dfs(x,y,count, rs):
-    global answer,numList
-    #print(x,y,count,rs)
-    rs.append(graph[x][y])
-    if count == 6:
-        #print("numlist",numList)
-        numInString = ''.join(map(str,rs))
-        if numInString not in numList:
-            answer +=1
-            numList.append(numInString) 
+graph = [list(map(int, input().rstrip().split())) for _ in range(5)]
+
+dx = [0,1,0,-1]
+dy = [1,0,-1,0]
+visited = set()
+answer = 0 
+def dfs(x,y,rs,depth):
+    global answer 
+    if depth == 6: 
+        tmp = ''.join(map(str,rs))
+        if tmp not in visited:
+            visited.add(tmp)
+            answer += 1 
         return 
+    
     for i in range(4):
         xx = x + dx[i]
         yy = y + dy[i]
-        if 0<= xx <5 and 0<= yy <5:
-            dfs(xx,yy,count+1, rs[:]) 
+        if 0<= xx<5 and 0<= yy <5:
+            rs.append(graph[xx][yy])
+            dfs(xx,yy,rs,depth+1)
+            rs.pop()
 
-numList = []
-graph = []
-for _ in range(5):
-    row = list(map(int,input().rstrip().split())) 
-    graph.append(row)
+rs = []
+for x in range(5):
+    for y in range(5):
+        rs.append(graph[x][y]) 
+        dfs(x,y,rs,1)
+        rs.pop()
 
-answer = 0 
-dx = [0,1,0,-1]
-dy = [1,0,-1,0]
-
-for i in range(5):
-    for j in range(5):
-        dfs(i,j,1,[])
-#print(numList)
 print(answer)
