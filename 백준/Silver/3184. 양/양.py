b319b8c8188eebe54793ba46d37e65a7a4db3f40
Minjun Kim(김민준) <101498204+kidmillionaire1229@users.noreply.github.com>
2024-02-20 21:22:33 +0900

    [Silver I] Title: 양, Time: 192 ms, Memory: 117600 KB -BaekjoonHub

--- 백준/Silver/3184. 양/양.py
+++ 백준/Silver/3184. 양/양.py
@@ -1,31 +1,64 @@
-import sys
-sys.setrecursionlimit(100000)
+# 점 (.) : 빈 칸 
+# # : 울타리 
+# o : 양 
+# v : 늑대 
 
-def DFS(x, y):
-    global wolf, sheep
+# 울타리 지나지 않고 다른 칸 이동 가능 
+#탈출 : 어떤 영역에서도 속하지 x 
+
+#늑대 < 양 : 양 win , 늑대 out 
+#else : 모든 양 먹음 
+
+import sys 
+input = sys.stdin.readline 
+from collections import deque
+
+#행, 열 
+n, m = map(int, input().rstrip().split())
+
+graph = [list(input().rstrip()) for _ in range(n)]
+
+#방향 
+dx = [0,1,0,-1]
+dy = [1,0,-1,0]
+
+wolf = 'v'
+sheep = 'o'
+
+def bfs(x,y):
+    global totalSheep, totalWolf
+    tempDict = {wolf:0, sheep:0}
+    #초기 설정 
     visited[x][y] = True
-    if farm[x][y] == 'v':
-        wolf += 1
-    elif farm[x][y] == 'o':
-        sheep += 1
-    for dx, dy in d:
-        nx, ny = x + dx, y + dy
-        if nx < 0 or ny < 0 or nx >= R or ny >= C: continue
-        if farm[nx][ny] == '#' or visited[nx][ny]: continue
-        DFS(nx, ny)
+    q = deque()
+    q.append((x,y))
 
+    if graph[x][y] != '.':
+        tempDict[graph[x][y]] += 1 
 
-R, C = map(int, input().split()) # 행 열
-farm = [list(input()) for _ in range(R)]
-visited = [[False] * C for _ in range(R)]
-totalwolf, totalsheep = 0, 0
-d = [[1, 0], [-1,0], [0, 1], [0, -1]]
-for i in range(R):
-    for j in range(C):
-        if farm[i][j] == "#" or visited[i][j]: continue
-        wolf, sheep = 0, 0
-        DFS(i, j)
-        if wolf >= sheep: totalwolf += wolf
-        else: totalsheep += sheep
+    while q:
+        x,y = q.popleft()
+        for i in range(4):
+            xx = x + dx[i]
+            yy = y + dy[i]
+            if 0<= xx <n and 0<= yy<m and not visited[xx][yy] and graph[xx][yy] != '#':
+                visited[xx][yy] = True
+                q.append((xx,yy))
+                if graph[xx][yy] != '.':
+                    tempDict[graph[xx][yy]] += 1 
+    
+    if tempDict[sheep] > tempDict[wolf]:
+        totalSheep += tempDict[sheep]
+    else: 
+        totalWolf += tempDict[wolf]
 
-print(totalsheep, totalwolf)
+#main 
+visited = [[False] * m for _ in range(n)]
+
+totalWolf , totalSheep = 0,0 
+for x in range(n):
+    for y in range(m):
+        if graph[x][y] != '#' and not visited[x][y]:
+            bfs(x,y)
+
+print(totalSheep, totalWolf)
