ef2480f0bcc1d7ac8bf100596fa581dc248417dd
Minjun Kim(김민준) <101498204+kidmillionaire1229@users.noreply.github.com>
2024-02-23 11:45:23 +0900

    [Gold IV] Title: 플로이드, Time: 192 ms, Memory: 111736 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -1,38 +1,29 @@
 import sys 
 input = sys.stdin.readline 
 
-n = int(input().rstrip())
-m = int(input().rstrip())
-INF = 1e9
+INF = int(1e9) 
+v = int(input().rstrip())
+e = int(input().rstrip())
 
-graph = [[INF] * (n+1) for _ in range(n+1)]
-
-for _ in range(m):
-    start, end, weight = map(int, input().rstrip().split())
-    graph[start][end] = min(graph[start][end], weight) 
-    #print("cehck",start, end, weight, graph[start][end])
-
-
-for i in range(1,n+1):
-    for j in range(1,n+1): 
+graph = [[INF] * (v+1) for _ in range(v+1)]
+for i in range(1,v+1):
+    for j in range(1,v+1):
         if i == j: 
             graph[i][j] = 0 
 
+for _ in range(e):
+    a, b, c = map(int,input().rstrip().split())
+    graph[a][b] = min(graph[a][b],c) 
 
-for k in range(1,n+1): 
-    for i in range(1,n+1): 
-        for j in range(1,n+1):
-            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j]) 
+for k in range(1,v+1):
+    for i in range(1,v+1):
+        for j in range(1,v+1):
+            graph[i][j] = min(graph[i][j],graph[i][k] + graph[k][j])
 
-for i in range(1,n+1):
-    for j in range(1,n+1):
-        if j == n:
-            if graph[i][j] == INF: 
-                print(0)
-            else:
-                print(graph[i][j])
-        else:
-            if graph[i][j] == INF:
-                print(0,end=" ")
-            else:
-                print(graph[i][j],end=" ")
+for i in range(1,v+1):
+    for j in range(1,v+1):
+        if graph[i][j] == INF:
+            print(0,end=" ")
+        else: 
+            print(graph[i][j],end=" ")
+    print()
