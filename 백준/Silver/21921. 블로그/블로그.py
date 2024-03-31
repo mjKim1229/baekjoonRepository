e7de893f3d28e88ed2665dfb428bf746defdd1f8
Minjun Kim(김민준) <101498204+mjKim1229@users.noreply.github.com>
2024-03-31 22:21:32 +0900

    [Silver III] Title: 블로그, Time: 188 ms, Memory: 182780 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.py
+++ 백준/Silver/21921. 블로그/블로그.py
@@ -2,27 +2,23 @@ import sys
 input = sys.stdin.readline 
 
 n, x = map(int, input().rstrip().split())
-array = list( map(int, input().rstrip().split()) )
+array = list(map(int, input().rstrip().split()))
 
-answer = [] 
-sum_value = 0 
-dayCount = 0
- 
-end = 0 
-for start in range(n):
-    while dayCount < x and end <n: 
-        sum_value += array[end]
-        dayCount += 1 
-        end += 1
-    if dayCount == x:
-        answer.append(sum_value)
-    dayCount -= 1 
-    sum_value -= array[start]
+dsum = [0]
+temp = 0 
+for i in array:
+    temp += i 
+    dsum.append(temp)
 
-maxNumber = max(answer)
-if maxNumber == 0: 
+answer = []
+for i in range(x,n+1):
+    tempSum = dsum[i]-dsum[i-x]
+    answer.append(tempSum)
+
+maxNum = max(answer)
+
+if maxNum == 0:
     print("SAD")
-else: 
-    print(maxNumber)
-    print(answer.count(maxNumber))
-        
+else:
+    print(maxNum)
+    print(answer.count(maxNum))
