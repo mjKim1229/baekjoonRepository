c41ca2b0e519025f9134d5db86a5b009edcd2ca0
Minjun Kim(김민준) <101498204+mjKim1229@users.noreply.github.com>
2024-10-18 21:19:56 +0900

    [Gold IV] Title: N-Queen, Time: 27120 ms, Memory: 203388 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,30 +1,30 @@
-import sys 
-input = sys.stdin.readline 
+from collections import defaultdict
 
-n = int(input().rstrip())
-board = [0] * n
-count = 0 
+N = int(input().rstrip())
+
+row = [0] * N 
 
 
-def promising(rowNum):
-    #print(board)
-    for i in range(rowNum): 
-        if board[i]== board[rowNum] or abs(board[i]-board[rowNum]) == abs(i-rowNum):
+def promising(x):
+    for i in range(x):
+        if row[i] == row[x] or abs(row[x] - row[i]) == (x - i):
             return False
+
     return True 
 
-def nqueen(rowNum):
-    #print("rowNum",rowNum)
-    global count  
-    if rowNum == n:
-        count+=1 
-        return  
-    
-    for i in range(n): 
-        board[rowNum] = i 
-        if promising(rowNum) == True:
-            nqueen(rowNum+1)
-    
-nqueen(0)
-print(count)
+
+count = 0 
+def backtrack(x):
+    global count
+    if x == N: 
+        count += 1
+        return 
+     
+    for i in range(N):
+        row[x] = i 
+        if promising(x):
+            backtrack(x + 1)
         
+
+backtrack(0)
+print(count)
