3a7148ccc2d5968fbaadf671e00387c406babd97
Minjun Kim(김민준) <101498204+mjKim1229@users.noreply.github.com>
2024-10-23 20:34:13 +0900

    [Silver III] Title: 블로그, Time: 144 ms, Memory: 160732 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.py
+++ 백준/Silver/21921. 블로그/블로그.py
@@ -6,31 +6,23 @@ N, X = map(int, input().rstrip().split())
 
 visitors = list(map(int, input().rstrip().split()))
 
-start, end = 0, X - 1 
-interval_sum = sum(visitors[start:end+1])
+start, end = 0, X  
+# 0 ~ X-1까지 
+interval_sum = sum(visitors[start:end])
 array = [interval_sum]
-start += 1 
-end += 1 
 
+ 
 while end < N:
-    interval_sum -= visitors[start-1]
+    interval_sum -= visitors[start]
     interval_sum += visitors[end]
     array.append(interval_sum)
+    
     start += 1 
     end += 1 
         
-    
-    
-
-#print(array)
 max_value = max(array)
 if max_value == 0: 
     print('SAD')
-    exit(0)
-
-print(max_value)
-print(array.count(max_value))
-
-
-
-
+else:
+    print(max_value)
+    print(array.count(max_value))
