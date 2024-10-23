a5c56c2f39622ad75a1856e4a6f6cf4c24c98c76
Minjun Kim(김민준) <101498204+mjKim1229@users.noreply.github.com>
2024-10-23 20:22:44 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 180 ms, Memory: 147064 KB -BaekjoonHub

--- 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.py
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.py
@@ -1,22 +1,43 @@
 import sys 
-from collections import defaultdict
 input = sys.stdin.readline 
+from collections import defaultdict
+#같은 원소가 K개 이하 최장 연속 부분 수열 
 
-n, k = map(int, input().rstrip().split())
+#길이 N , K개 이하 
 
-array = list(  map(int, input().rstrip().split()) )
-index = defaultdict(int)
+N, K = map(int, input().rstrip().split())
+array = list(map(int, input().rstrip().split()))
+count_dict = defaultdict(int)
 
-answer = 0 
+start, end = 0, 0 
+
+#넘어가면 True 
+def is_over():
+    for value in count_dict.values():
+        if value > K:
+            return True 
+    return False 
+
+
 left, right = 0, 0 
-
-while right < n:
-    if index[array[right]] < k: 
-        index[array[right]] +=1 
-        right +=1 
+max_value = 0 
+while right < N: 
+    if count_dict[array[right]] < K: 
+        count_dict[array[right]] += 1 
+        right += 1 
     else: 
-        index[array[left]] -= 1 
-        left +=1 
-    answer = max(answer,right-left)
+        count_dict[array[left]] -= 1 
+        left += 1 
+    max_value = max(right - left, max_value)
 
-print(answer)
+print(max_value)
+
+
+
+
+
+
+
+
+
+
