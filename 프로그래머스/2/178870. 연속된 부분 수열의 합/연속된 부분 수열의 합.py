d5a818fcd18be9ccc64a98baf16c3bb6dc56e349
Minjun Kim(김민준) <101498204+mjKim1229@users.noreply.github.com>
2024-10-24 15:43:30 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 1208.36 ms, Memory: 286 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
@@ -1,29 +1,18 @@
-temp = []
-answer = []
-
-def two_pointer(k, sequence):
-    left, right = 0, 0 
-    N = len(sequence)
+def solution(sequence, k):
+    answer = []
     interval_sum = 0 
+    N = len(sequence)
+    left, right = 0, 0 
     for left in range(N):
         while right < N and interval_sum < k:
             interval_sum += sequence[right]
             right += 1 
         
         if interval_sum == k: 
-            answer.append((left, right-1, right-left))
+            answer.append([left, right-1, right-left+1])
         
         interval_sum -= sequence[left]
-        left += 1 
     
-            
-            
-            
-        
-def solution(sequence, k):
-    two_pointer(k, sequence)
     answer.sort(key = lambda x : (x[2], x[0]))
-    final_answer = []
-    final_answer.append(answer[0][0])
-    final_answer.append(answer[0][1])
-    return final_answer
+    return [answer[0][0], answer[0][1]]           
+    
