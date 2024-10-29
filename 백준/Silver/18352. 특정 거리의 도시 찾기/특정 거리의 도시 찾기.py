9e2bea430e81c96b8a26a5636cdd2aa3d061c427
Minjun Kim(김민준) <101498204+mjKim1229@users.noreply.github.com>
2024-10-29 21:58:10 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 876 ms, Memory: 156028 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,36 +1,31 @@
 import sys 
 input = sys.stdin.readline 
+
 from collections import deque
 
-def bfs(x):
-    visited[x] = True 
-    q = deque()
-    q.append(x)
-    while q: 
-        x = q.popleft()
-        for i in graph[x]:
-            if not visited[i]:
-                visited[i] = True 
-                distance[i] = distance[x] + 1 
-                q.append(i)
+N, M, K, X = map(int, input().rstrip().split())
+graph = [[] for _ in range(N+1)]
 
-n, m , k, x = map(int, input().rstrip().split())
-graph = [ [] for _ in range(n+1)]
-
-for _ in range(m):
+for _ in range(M):
     a, b = map(int, input().rstrip().split())
     graph[a].append(b)
 
-visited = [False] * (n+1)
-distance = [0] * (n+1)
-bfs(x)
+distance = [-1] * (N+1)
+distance[X] = 0 
+q = deque()
+q.append(X)
 
+while q: 
+    now = q.popleft()
+    for next in graph[now]:
+        if distance[next] == -1:
+            q.append(next)
+            distance[next] = distance[now] + 1 
 
-isNone = True  
-for i in range(len(distance)):
-    if distance[i] == k: 
-        print(i)
-        isNone = False 
-
-if isNone:
+if distance.count(K) == 0: 
     print(-1)
+else: 
+    for i in range(len(distance)):
+        if distance[i] == K: 
+            print(i)
+
