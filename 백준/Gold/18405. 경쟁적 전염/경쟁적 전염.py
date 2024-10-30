ef8df94daf6c28ba5bf8faa4f17afb2dbe5e0ccb
Minjun Kim(김민준) <101498204+mjKim1229@users.noreply.github.com>
2024-10-30 14:50:50 +0900

    [Gold V] Title: 경쟁적 전염, Time: 152 ms, Memory: 116352 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
@@ -1,34 +1,41 @@
 import sys 
-input = sys.stdin.readline
+input = sys.stdin.readline 
+
 from collections import deque
+N, K = map(int, input().rstrip().split())
+graph = []
+virus = []
 
-#nxn그래프, k초 후 
-n, virusNum = map(int, input().rstrip().split())
-board = []
-initVirus = []
+for i in range(N):
+    graph.append(list(map(int, input().rstrip().split())))
+    for j in range(N):
+        if graph[i][j] != 0: 
+            virus.append((graph[i][j], i, j))
 
-for i in range(n):
-    board.append(list(map(int,input().rstrip().split())))
-    for j in range(n):
-        if board[i][j] != 0:
-            initVirus.append((board[i][j],i,j,0))
+S, X, Y = map(int, input().rstrip().split())
 
-target_time, target_X, target_Y = map(int,input().rstrip().split())
-initVirus.sort()
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
 
-destinationX = [0,1,0,-1]
-destinationY = [1,0,-1,0]
+def bfs(s, X, Y):
+    q = deque(virus)
+    count = 0
+    while q: 
+        if count == s: 
+            break 
+        for _ in range(len(q)):
+            prev, x, y = q.popleft()
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                if 0<= nx < N and 0<= ny < N: 
+                    if graph[nx][ny] == 0: 
+                        graph[nx][ny] = prev
+                        q.append((graph[nx][ny], nx, ny))
+        count += 1 
+    return graph[X-1][Y-1]
 
-q = deque(initVirus)
-while q: 
-    virus,x,y,time = q.popleft()
-    if time == target_time:
-        break
-    for i in range(4):
-        tempX = x + destinationX[i]
-        tempY = y + destinationY[i]
-        if 0<=tempX<n and 0<=tempY<n and board[tempX][tempY] == 0:
-            board[tempX][tempY] = virus
-            q.append((board[tempX][tempY],tempX,tempY,time+1))
+virus.sort()
+print(bfs(S, X, Y))
 
-print(board[target_X-1][target_Y-1])
+
