015a1e85589c0a264f876476d146d1cc10f65050
Minjun Kim(김민준) <101498204+mjKim1229@users.noreply.github.com>
2024-10-31 21:04:40 +0900

    [Gold V] Title: 최소 회의실 개수, Time: 420 ms, Memory: 124736 KB -BaekjoonHub

--- 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.py
+++ 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.py
@@ -1,32 +1,30 @@
-#N개의 회의 진행할 수 있는 최소 회의실 
-#시작 - 끝
-#회의의 시작 시간은 끝나는 시간보다 항상 작다.
-import sys 
+import sys, heapq
 input = sys.stdin.readline 
-import heapq
-n = int(input().rstrip())
-time = []
-for _ in range(n):
+
+N = int(input().rstrip())
+times = []
+
+for _ in range(N):
     start, end = map(int, input().rstrip().split())
-    time.append((start,end))
+    times.append([start, end])
 
-time.sort(key= lambda x : (x[0],x[1]))
+times.sort()
+q = []
+heapq.heappush(q, times[0][1]) 
 
-count = 0 
-minList = []
-
-#초기 값 
-start, end = time[0]
-heapq.heappush(minList,end)
-count += 1 
-
-
-for i in range(1,n):
-    start, end = time[i]
-    if start >= minList[0]:
-        heapq.heappop(minList)
-        heapq.heappush(minList,end)
-    else: 
+count = 1 
+for i in range(1, N):
+    start, end = times[i]
+    if start < q[0]:
         count += 1 
-        heapq.heappush(minList, end)
+        heapq.heappush(q, end)
+    else: 
+        heapq.heappop(q)
+        heapq.heappush(q, end)
+
 print(count)
+
+
+
+
+
