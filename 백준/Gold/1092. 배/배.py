8cd18be517a6588a7fae4268335192926d5dc231
Minjun Kim(김민준) <101498204+mjKim1229@users.noreply.github.com>
2024-10-31 21:48:06 +0900

    [Gold V] Title: 배, Time: 108 ms, Memory: 110404 KB -BaekjoonHub

--- 백준/Gold/1092. 배/배.py
+++ 백준/Gold/1092. 배/배.py
@@ -1,43 +1,29 @@
-#크레인 N대 
-#1분에 박스를 하나씩 배에 싣는다. 
-#모든 크레인 동시에 
-import sys 
-input = sys.stdin.readline 
-import heapq
+import sys
+input = sys.stdin.readline
 
-n = int(input().rstrip())
-cranes = list(map(int, input().rstrip().split()))
+N = int(input())
+cranes = list(map(int, input().split()))
+M = int(input())
+boxes = list(map(int, input().split()))
+
 cranes.sort(reverse=True)
-m = int(input().rstrip())
-boxes = list(map(int, input().rstrip().split()))
 boxes.sort(reverse=True)
 
-if boxes[0] > cranes[0]:
-    print(-1)
-    sys.exit(0)
+cnt = 0 
 
-count =0 
-while boxes:
-    for c in cranes:
-        #다음 크레인 갖고와야함 : 뒤에 box들도 똑같이 다 처리 못함 
-        if boxes and c < boxes[-1]:
-            continue
-        #지금 크레인으로 무조건 하나 처리 가능함 
-        #처리 시 => break 
-        for b in boxes:
-            if c >= b: 
-                boxes.remove(b)
-                break
-    count += 1
+if boxes[0] > cranes[0]: 
+    cnt = -1
+else:
+    while boxes:
+        for c in cranes:
+            if boxes and c < boxes[-1]:
+                continue
+            for b in boxes:
+                if c >= b: 
+                    boxes.remove(b)
+                    break 
+        cnt += 1 
 
-print(count)
+print(cnt)
 
-   
-
-#https://www.acmicpc.net/board/view/122007
-
-# 2
-# 1 2
-# 4
-# 1 1 2 2
 
