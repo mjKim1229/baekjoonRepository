55da7345ae643b6c8cae233c1668cf323af71c1d
Minjun Kim(김민준) <101498204+mjKim1229@users.noreply.github.com>
2024-10-31 19:55:45 +0900

    [Silver V] Title: 뒤집기, Time: 92 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.py
+++ 백준/Silver/1439. 뒤집기/뒤집기.py
@@ -1,18 +1,15 @@
 import sys 
 input = sys.stdin.readline 
 
+S = input().rstrip()
 
-num = input().rstrip()
-countList = [0,0]
+number_dict = {'1' : 0 , '0' : 0} 
 
-first = int(num[0])
-countList[first] +=1 
-before = first 
+for i in range(len(S)):
+    if i == 0: 
+        number_dict[S[i]] += 1 
+    else: 
+        if S[i - 1] != S[i]:
+            number_dict[S[i]] += 1 
 
-for i in range(1,len(num)):
-    now = int(num[i])
-    if now != before:
-        countList[now] +=1 
-        before = now 
-
-print(min(countList))
+print(min(number_dict['1'], number_dict['0']))
