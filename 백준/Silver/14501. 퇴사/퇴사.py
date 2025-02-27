1c81d666251e7eaaa057d3301faa61793ecc8419
Minjun Kim(김민준) <101498204+mjKim1229@users.noreply.github.com>
2025-02-27 23:07:41 +0900

    [Silver III] Title: 퇴사, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,24 +1,24 @@
-n = int(input()) # 전체 상담 개수
-t = [] # 각 상담을 완료하는데 걸리는 기간
-p = [] # 각 상담을 완료했을 때 받을 수 있는 금액
-dp = [0] * (n + 1) # 다이나믹 프로그래밍을 위한 1차원 DP 테이블 초기화
-max_value = 0
+import sys 
+input = sys.stdin.readline 
 
-for _ in range(n):
-    x, y = map(int, input().split())
+# 1 ~ N일 // N+1일 퇴사 //0은 버리는 인덱스 
+N = int(input().rstrip())
+t = [0]
+p = [0]
+dp = [0] * (N + 2)
+max_value = 0 
+for _ in range(N):
+    x, y = map(int, input().rstrip().split())
     t.append(x)
     p.append(y)
 
-# 리스트를 뒤에서부터 거꾸로 확인
-for i in range(n - 1, -1, -1):
-    time = t[i] + i
-    # 상담이 기간 안에 끝나는 경우
-    if time <= n:
-        # 점화식에 맞게, 현재까지의 최고 이익 계산
-        dp[i] = max(p[i] + dp[time], max_value)
+max_value = 0 
+for i in range(N, 0, -1):
+    time = i + t[i]
+    if time <= (N + 1):
+        dp[i] = max(max_value, dp[time] + p[i]) 
         max_value = dp[i]
-    # 상담이 기간을 벗어나는 경우
-    else:
+    else: 
         dp[i] = max_value
 
 print(max_value)
