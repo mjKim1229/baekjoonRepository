bf88928dcec0b12fb79a407018cf1d0f104c55c7
Minjun Kim(김민준) <101498204+mjKim1229@users.noreply.github.com>
2025-03-18 18:33:38 +0900

    [Gold IV] Title: 무기 공학, Time: 520 ms, Memory: 121968 KB -BaekjoonHub

--- 백준/Gold/18430. 무기 공학/무기 공학.py
+++ 백준/Gold/18430. 무기 공학/무기 공학.py
@@ -7,50 +7,55 @@ array = [list(map(int, input().rstrip().split())) for _ in range(N)]
 visited = [[False] * M for _ in range(N)]
 result = 0 
 
+#첫번째 
 dx = [(0, 1), (0, 1), (0, -1), (0, -1)]
+#두번째 
 dy = [(1, 0), (-1, 0), (1, 0), (-1, 0)]
 
-def in_area(x1, y1, x2, y2):
-    if 0<= x1 < N and 0<= y1 <M and 0<= x2 <N and 0<= y2 <M:
+def in_area(x, y):
+    if 0<= x < N and 0<= y <M: 
         return True
     return False
 
 def get_score(x, y, x1, y1, x2, y2):
     return array[x][y] * 2 + array[x1][y1] + array[x2][y2]
 
+def get_nx(x, y):
+    if y == (M-1):
+        return x+1, 0 
+    else: 
+        return x, y + 1
 
-def dfs(x, y, count):
+result = 0 
+def dfs(x, y, score):
     global result
-    result = max(result, count)
-
+    result = max(result, score)
+    
     if x == N: 
-        return
+        return 
 
-    if not visited[x][y]: 
+    nx, ny = get_nx(x, y)
+    
+    if not visited[x][y]:
         for i in range(4):
             x1, y1 = x + dx[i][0], y + dx[i][1]
             x2, y2 = x + dy[i][0], y + dy[i][1]
-
-            if in_area(x1, y1, x2, y2):
-                if not visited[x1][y1] and not visited[x2][y2]: 
-                    visited[x1][y1] = True
-                    visited[x2][y2] = True
+            
+            #넣고 처리 
+            if in_area(x1, y1) and in_area(x2, y2):
+                if not visited[x1][y1] and not visited[x2][y2]:
                     visited[x][y] = True
-
-                    #열 맨 아래까지 도착 -> 행 전환 
-                    if y == M-1: 
-                        dfs(x+1, 0, count + get_score(x, y, x1, y1, x2, y2))
-                    else: 
-                        dfs(x, y+1, count + get_score(x, y, x1, y1, x2, y2))
-
+                    visited[x1][y1] = True
+                    visited[x2][y2] = True 
+                    dfs(nx, ny, score + get_score(x, y, x1, y1, x2, y2))
+                    visited[x][y] = False
                     visited[x1][y1] = False
                     visited[x2][y2] = False
-                    visited[x][y] = False
-                
-    if y == M-1:
-        dfs(x+1, 0, count)
-    else: 
-        dfs(x, y+1, count)
+            
+    #안넣고 처리 
+    dfs(nx, ny, score)
 
-dfs(0,0,0)
+
+dfs(0, 0, 0)
+
 print(result)
